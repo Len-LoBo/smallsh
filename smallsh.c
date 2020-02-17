33f563a4a3e56eb35ac18d9f0f1f687603a4783b
Leonard LoBiondo <lobiondl@os1.engr.oregonstate.edu>
2020-02-17 12:36:31 -0800

    Foreground processes working, ERROR if invalid command sent.  cd working

--- smallsh.c
+++ smallsh.c
@@ -10,9 +10,13 @@ char* getUserInput();
 void exitShell();
 void changeDir(char*);
 void getStatus();
+void execute(char**);
 
 void main()
 {
+    pid_t spawnPid = -5;
+    int childExitMethod = -5;
+
     // string for storing user input and tokenizing
     char* userInput;
     char* token;
@@ -25,33 +29,69 @@ void main()
     //prints prompt to screen
     while (1)
     {
+        	
+	char* args[513];
+	int argCounter = 0;
+
+	//initializes args array pointers to NULL
+	for (int i = 0; i < 513; i++)
+	{
+	    args[i] = NULL;
+	}
+	
+	//prompts user for command line input
         prompt(); 
+
+	//stores user input
         userInput = getUserInput();
-        token = strtok(userInput, " \n");
-	char* args[512];
-    
+        
+	//tokenizes input to get commands, stores in array position 0
+        token = strtok(userInput, " \n");	
+	args[argCounter] = token;
+        argCounter++;	
+	
+	//stores remaining arguments in argument array
+	while ((token = strtok(NULL, " \n")) != NULL)
+	{ 
+            args[argCounter] = token;
+	    argCounter++;
+	}
+        
+
 	//checks for built in commands
-        if (strcmp(token, exitText) == 0)
+        if (strcmp(*args, exitText) == 0)
         {
             exitShell();
         }
-	else if (strcmp(token, changeDirText) == 0)
+	else if (strcmp(*args, changeDirText) == 0)
 	{
-	    token = strtok(NULL, " \n");
-            changeDir(token);
+	    if (args[1] == NULL) changeDir(getenv("HOME"));
+	    else changeDir(args[1]);
  	}
-	else if (strcmp(token, statusText) == 0)
+	else if (strcmp(*args, statusText) == 0)
 	{
 	    getStatus();
 	}
-
-	//non built in command attempted
-	else
+	else 
 	{
-
+	    //fork() child 
+	    spawnPid = fork();
+	    //if fork fails -- ERROR
+	    switch (spawnPid)
+	    {
+	        case -1: { perror("Process fork() failed!\n"); }	
+	        case 0: {
+	                //printf("Child process created.\n"); 
+			execute(args);
+			perror("Exec failure");
+	        }
+	        default: {
+	                //waits for child process to terminate
+	                waitpid(spawnPid, &childExitMethod, 0);
+	        }
+	    }
 	}
     }
-
 }
 
 //prompt() --  displays the command line prompt
@@ -111,5 +151,12 @@ void getStatus()
     printf("getstatus");
 }
 
-
-
+// execute() -- executes a program using execvp, prints error if exec fails
+void execute(char** argv)
+{
+    if (execvp(*argv, argv) < 0)
+    {
+        perror("Command Exec failure!");
+	exit(1);
+    }
+}
