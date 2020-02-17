b6c0625b773bfa34f6be56e13f9ed1090528c6e7
Leonard LoBiondo <lobiondl@os1.engr.oregonstate.edu>
2020-02-17 15:06:52 -0800

    File redirect working. Status command working.  Moving on to replace PID 47415

--- smallsh.c
+++ smallsh.c
@@ -2,20 +2,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
+#include <fcntl.h>
 
 void prompt();
 char* getUserInput();
 void exitShell();
 void changeDir(char*);
-void getStatus();
+void getStatus(int, int);
 void execute(char**);
 
 void main()
 {
     pid_t spawnPid = -5;
     int childExitMethod = -5;
+    int lastExitStatus = 0;
+    int exitType = 0;
 
     // string for storing user input and tokenizing
     char* userInput;
@@ -51,7 +55,7 @@ void main()
         argCounter++;	
 	
 	//stores remaining arguments in argument array
-	while ((token = strtok(NULL, " \n")) != NULL)
+	while ((token = strtok(NULL, " \n")) != NULL && strcmp(token, "<") != 0 && strcmp(token, ">") != 0) 
 	{ 
             args[argCounter] = token;
 	    argCounter++;
@@ -70,8 +74,9 @@ void main()
  	}
 	else if (strcmp(*args, statusText) == 0)
 	{
-	    getStatus();
+	    getStatus(lastExitStatus, exitType);
 	}
+	// if not build in command
 	else 
 	{
 	    //fork() child 
@@ -81,13 +86,70 @@ void main()
 	    {
 	        case -1: { perror("Process fork() failed!\n"); }	
 	        case 0: {
+			//perform redirection
+			while (token != NULL)
+			{
+
+		            if (strcmp(token, ">") == 0)
+			    {
+		                token = strtok(NULL, " \n");
+
+				int targetFD = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+				if (targetFD == -1) 
+				{
+				    perror("open()");
+			            exit(1); 
+				}
+				else
+				{
+				    int result = dup2(targetFD, 1);
+				    if (result == -1) { 
+			                perror("dup2_stdout"); 
+					exit(2);
+				    }
+				    
+				}
+			    }
+		            else if (strcmp(token, "<") == 0)	
+			    {
+		                token = strtok(NULL, " \n");
+
+				int sourceFD = open(token, O_RDONLY);
+
+				if (sourceFD == -1) { 
+				    perror("open()"); 
+				    exit(1);
+				}
+				
+				else 
+			        {
+			            int result = dup2(sourceFD, 0);
+			            if (result == -1) { 
+				        perror("dup2_stdin"); 
+					exit(2);
+				    }	
+				}
+			    }
+			    token = strtok(NULL, " \n");
+			}
+			
 	                //printf("Child process created.\n"); 
 			execute(args);
 			perror("Exec failure");
 	        }
 	        default: {
 	                //waits for child process to terminate
-	                waitpid(spawnPid, &childExitMethod, 0);
+	                pid_t childPid = waitpid(spawnPid, &childExitMethod, 0);
+			if (childPid == -1)
+			{
+			    perror("wait failed");
+			}
+	                if (WIFEXITED(childExitMethod))
+			{
+		            lastExitStatus = WEXITSTATUS(childExitMethod);
+			    exitType = 0;
+			}
 	        }
 	    }
 	}
@@ -100,6 +162,7 @@ void prompt()
     printf(": ");
     fflush(stdout);
 }
+
 
 //getUserInput() -- gets user input from stdin, and returns a string
 char* getUserInput()
@@ -146,9 +209,18 @@ void changeDir(char* path)
 
 
 //getStatus() -- returns exit code or signel number of most recently terminated child process
-void getStatus()
+void getStatus(int lastExitStatus, int exitType)
 {
-    printf("getstatus");
+    if (exitType == 0)
+    {
+        printf("exit value %d\n", lastExitStatus);
+        fflush(stdout);
+    }
+    else
+    {
+        printf("exited by signal");
+	fflush(stdout);
+    }
 }
 
 // execute() -- executes a program using execvp, prints error if exec fails
@@ -159,4 +231,10 @@ void execute(char** argv)
         perror("Command Exec failure!");
 	exit(1);
     }
+}
+
+char* replacePID(char* text)
+{
+    char buffer[255];
+
 }
